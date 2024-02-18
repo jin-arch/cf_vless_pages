1ffa4dbbe08a8d96db2c7fa52d5b52e4c8d3fdda
jin-arch <54517832+jin-arch@users.noreply.github.com>
2024-02-18 13:20:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,12 +11,12 @@ let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给
 //let sub = '';// 留空则显示原版内容
 let sub = '200848a7-a9be-4546-bcc4-79b9bb10304a.cmliussss.workers.dev';// 内置优选订阅生成器，可自行搭建 https://github.com/cmliu/WorkerVless2sub
 let subconverter = 'api.v1.mk';// clash订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://github.com/jin-arch/cf_vless_pages/raw/main/Clash/config/Online_Full_Auto.ini"; //订阅配置文件
+let subconfig = "https://raw.githubusercontent.com/cmliu/edgetunnel/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
 // Example:  user:pass@host:port  or  host:port
 let socks5Address = '';
-let RproxyIP = "false";
+let RproxyIP = 'false';
 if (!isValidUUID(userID)) {
 	throw new Error('uuid is not valid');
 }
@@ -40,8 +40,9 @@ export default {
 			sub = env.SUB || sub;
 			subconverter = env.SUBAPI || subconverter;
 			subconfig = env.SUBCONFIG || subconfig;
-			RproxyIP = env.RPROXYIP || RproxyIP;
+			//RproxyIP = env.RPROXYIP || !proxyIP ? 'true' : 'false';
 			if (socks5Address) {
+				RproxyIP = env.RPROXYIP || 'false';
 				try {
 					parsedSocks5Address = socks5AddressParser(socks5Address);
 					enableSocks = true;
@@ -50,6 +51,8 @@ export default {
 					console.log(e.toString());
 					enableSocks = false;
 				}
+			} else {
+				RproxyIP = env.RPROXYIP || !proxyIP ? 'true' : 'false';
 			}
 			const upgradeHeader = request.headers.get('Upgrade');
 			const url = new URL(request.url);
@@ -59,7 +62,7 @@ export default {
 				case '/':
 					return new Response(JSON.stringify(request.cf), { status: 200 });
 				case `/${userID}`: {
-					const vlessConfig = await getVLESSConfig(userID, request.headers.get('Host'), sub, userAgent);
+					const vlessConfig = await getVLESSConfig(userID, request.headers.get('Host'), sub, userAgent, RproxyIP);
 					return new Response(`${vlessConfig}`, {
 					status: 200,
 					headers: {
@@ -781,8 +784,7 @@ function socks5AddressParser(address) {
  * @param {string} userAgent
  * @returns {Promise<string>}
  */
-async function getVLESSConfig(userID, hostName, sub, userAgent, proxyIP) {
-	if (!proxyIP || proxyIP === '') RproxyIP = "true";
+async function getVLESSConfig(userID, hostName, sub, userAgent, RproxyIP) {
 	// 如果sub为空，则显示原始内容
 	if (!sub || sub === '') {
 		const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`;
@@ -818,8 +820,7 @@ async function getVLESSConfig(userID, hostName, sub, userAgent, proxyIP) {
 	
 		return `
 	################################################################
-	Subscribe / sub
-	响应式订阅链接, 支持 Base64、clash-meta、sing-box 订阅格式, 您的订阅内容由 ${sub} 提供维护支持.
+	Subscribe / sub 订阅地址, 支持 Base64、clash-meta、sing-box 订阅格式, 您的订阅内容由 ${sub} 提供维护支持, 自动获取ProxyIP: ${RproxyIP}.
 	---------------------------------------------------------------
 	https://${hostName}/${userID}
 	---------------------------------------------------------------
